2f9c80735ef88660ac01da2c648769b409bc6ab0
MrRaccoon58 <33047306+MrRaccoon58@users.noreply.github.com>
2025-06-05 02:44:42 +0200

    Add simple React website

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"# turbo-pancake" 
+# Turbo Pancake
+
+This repository contains a simple React website in the `website` directory.
+Open `website/public/index.html` in your browser to view the homepage.
---
+++ website/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{textAlign: 'center', marginTop: '50px'}}>
+      <h1>Welcome to React Homepage</h1>
+      <p>This is a basic React homepage.</p>
+    </div>
+  );
+}
+
+export default App;
